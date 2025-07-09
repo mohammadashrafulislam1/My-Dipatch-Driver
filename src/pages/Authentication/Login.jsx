62048338cedc43c55413ded2189f5751cbea69a4
mohammadashrafulislam1 <ixdell41@gmail.com>
2025-07-09 02:16:48 -0500

    changed logo

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-center bg-[#F4F9FF] px-4">
       <div className="w-full max-w-md bg-white p-6 sm:p-4 rounded-2xl shadow-xl">
         <img
-          src="https://i.ibb.co/Fkwr6fBV/IMG-7276-removebg-preview.png"
+          src="https://i.ibb.co/TxC947Cw/thumbnail-Image-2025-07-09-at-2-10-AM-removebg-preview.png"
           alt="Logo"
           className="w-[120px] sm:w-[150px] mx-auto mb-6"
         />
--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -27,7 +27,7 @@ const Signup = () => {
     <div className="min-h-screen flex items-center justify-center bg-[#F4F9FF] px-4">
       <div className="w-full max-w-md bg-white md:p-8 p-4 rounded-2xl shadow-2xl">
         <img
-          src="https://i.ibb.co/Fkwr6fBV/IMG-7276-removebg-preview.png"
+          src="https://i.ibb.co/TxC947Cw/thumbnail-Image-2025-07-09-at-2-10-AM-removebg-preview.png"
           alt="Logo"
           className="w-[150px] mx-auto mb-6"
         />
